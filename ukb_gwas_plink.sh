ce19db00ca899210ea3fa82c9c31722e8040f793
aprilhartley <77832842+aprilhartley@users.noreply.github.com>
2022-07-05 16:55:16 +0100

    Create ukb_gwas_plink.sh

---
+++ ukb_gwas_plink.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -p mrcieu,mrcieu2
+
+# load anaconda for pandas
+module load languages/anaconda3/3.7
+
+# read .env file
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo "No .env file exists, exiting"
+  exit
+fi
+
+# set default job row to 0
+JOB="${1:-0}"
+
+USER=$(whoami)
+
+# QC the pheno and covariate files
+python scripts/qc.py -j $JOB -p $PIPELINE_DATA -u $USER
+
+# Create and run the gwas script
+python scripts/create_gwas_job_plink.py -j $JOB -p $PIPELINE_DATA -u $USER -d $UKBIOBANK_DATA
